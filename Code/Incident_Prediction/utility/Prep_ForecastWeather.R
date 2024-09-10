df8be77c252f6ad445364ff1b7b84f19400ff46e
Joey Reed <112653410+jreed-volpe@users.noreply.github.com>
2024-09-10 17:10:36 -0400

    [NEW] Updated Prep_ForecastWeather.R
    Fully functional. Creates a dataframe called "weather_forecast" which has osm_id and hour to merge with the predictweek.R dataframe. Merge still needs to be configured when predictweek.R is being worked on.
    
    Get_weather_forecast still has parameters in it that will need to be deleted eventually.

--- Code/Incident_Prediction/utility/Prep_ForecastWeather.R
+++ Code/Incident_Prediction/utility/Prep_ForecastWeather.R
@@ -1,224 +1,48 @@
 # Intro -----------------------------------
+# Purpose: This script sources Get_weather_forecast.R then spatial joins those results with the osm network
 
-# Run from PredictWeek.R, following Get_weather_forecasts.R, which provides weather_hourly.proj 
-# already in memory: inputdir, teambucket, codeloc, state, and g, which repreresents the grid type to use
+library(RANN) # holds nn2 
 
-# Also already in memory is `next_week`, a data frame of the grid ID and date/time for the next week
-
-inputdir <- file.path(getwd(),"Input")
-outputdir<- file.path(getwd(),"Output")
-
-state <- "WA"
-
-year <- 2021
-
-# Indicate whether the state has a unified time zone
-one_zone <-TRUE
-# If one_zone is set to T or TRUE, meaning that the state has one time zone, specify the name of the time zone, selecting from 
-# among the options provided after running the first line below (OlsonNames())
-
-# OlsonNames()
-# time_zone_name <- "US/Central"
-time_zone_name <- "US/Pacific"
-
-# projection used in training is epsg 5070, so using that here as well. 
-projection <- 5070
-
-# The coordinate reference system for the TomorrowIO API is WGS84 (epsg code 4326)
-api_crs <- 4326
-
-library(gstat) # For kriging
-library(rgeos) # for gIntersects
-library(sf)
-library(raster) # masks several functions from dplyr, use caution
-library(doParallel)
-library(foreach)
+# REMOVE THIS ONCE PULLING INTO PREDICTWEEK.R (WILL ALSO NEED TO CHANGE roads THROUGHOUT HERE)
+roads <- st_read(file.path(inputdir, "Roads_Boundary", "Washington_State", "Washington_State_network.gpkg", "Washington_State_network.shp"))
+#load("D:/Documents/Joey/R25-IncidentDetection/Code/Incident_Prediction/Input/Weather/Weather_Forecasts_WA_2024-09-10.RData")
 
 prepname = paste0("OSM_Weather_", state, "_", Sys.Date(), ".RData")
 
 if(!file.exists(file.path(inputdir, 'Weather', prepname))) {
 
-  cat("Preparing Forecasts to", as.character(max(next_week$date)), "for", g, "\n")
-  
   # Get weather forecast for the next week
   # First check to see if forecast has been run already from today (to not over-user API call)
-  
+
   if(file.exists(file.path(inputdir, 'Weather', paste0("Weather_Forecasts_", state, "_", Sys.Date(), ".RData")))) {
     load(file.path(inputdir, 'Weather', paste0("Weather_Forecasts_", state, "_", Sys.Date(), ".RData")))
   } else {
     source('datacleaning/Get_weather_forecasts.R')
   }
-  
-  
-  # Interpolate over state ----
-  # http://rspatial.org/analsis/rst/4-interpolation.html
-  # First, make this a spatial points data frame
-  # Project crashes
 
-  wx.proj <- spTransform(wx_dat.proj, CRS(proj.USGS))
-  
-  # Read in grid
-  # grid_shp <- rgdal::readOGR(file.path(inputdir, "Shapefiles"), layer = g)
-  grid_shp <- read_sf(file.path(inputdir, "Shapefiles"), layer = g)
-  grid_shp <- spTransform(grid_shp, CRS(proj.USGS))
-  
-  # Read in buffered state shapefile
-  tn_buff <- read_sf(censusdir, layer = "TN_buffered")
-  tn_buff <- spTransform(tn_buff, CRS(proj.USGS))
-  
-  # Clip grid to county shapefile
-  grid_intersects <- gIntersects(tn_buff, grid_shp, byid = T)
-  
-  grid_shp <- grid_shp[as.vector(grid_intersects),]
-  
-  wx <- wx_dat %>%
-    mutate(local_time = as.POSIXct(local_time),
-           mo = format(local_time, "%m"),
-           date = format(local_time,'%Y-%m-%d'))
-  
-  wx.proj@data <- wx.proj@data %>%
-    mutate(local_time = as.POSIXct(local_time),
-           mo = format(local_time, "%m"),
-           date = format(local_time, '%Y-%m-%d'))
-  
-  # Kriging. Intercept only = ordinary kriging.
-  # Parallize by day -- only need up to 7 cores
-  use.cores = ifelse(parallel::detectCores() > 7, 7, parallel::detectCores()) 
-  cl <- makeCluster(use.cores)
-  registerDoParallel(cl)
-  
-  # Create an empty raster grid for the state; we will interpolate over this grid, then assign values from the raster to each grid cell. Increase n for smaller raster cells (more time-intensive)
-  grd <- as.data.frame(spsample(grid_shp, 'regular', n = 10000))
-  names(grd) <- c("X", "Y")
-  coordinates(grd) <- c("X", "Y")
-  gridded(grd) <- TRUE # for SpatialPixel
-  fullgrid(grd) <- TRUE # for SpatialGrid
-  proj4string(grd) <- proj4string(grid_shp)
-  
-  StartTime <- Sys.time()
-  
-  writeLines(c(""), paste("Prep_Weather_to", max(next_week$date), g, "log.txt", sep="_"))    
-  
-  # Start parallel loop ----
-  # limit to dates in the set of months we are using, see do.months specified in the RandomForest_WazeGrids file.
-  all_wx_days = unique(wx$date)
- 
-  wx.grd.day <- foreach(day = all_wx_days, 
-                      .packages = c('raster','gstat','dplyr','rgdal'), 
-                      .combine = rbind) %dopar% {
-    cat(paste(Sys.time()), as.character(day), "\n", 
-        file = paste("Prep_Weather_to", max(next_week$date), g, "log.txt", sep="_"), append = T) 
-        
-    # Scan team bucket for completed daily weather prep ----
-    fn = paste("Prep_Weather_Daily_", day,"_", g, ".RData", sep="")
-    
-    # See if exists in outputdir. Load if so. If not, carry out kriging steps.
-    if(file.exists(file.path(file.path(inputdir,"Weather", fn)))){
-      load(file.path(inputdir,"Weather", fn))
-    } else {
-    
-    wx.day = wx.proj[wx.proj$date == day,]
-                        
-    f.p <- as.formula(precipIntensityMax  ~ 1)
-    
-    vg_prcp <- gstat::variogram(precipIntensityMax  ~ 1, locations = wx.day[!is.na(wx.day$precipIntensityMax ),])
-    dat.fit <- fit.variogram(vg_prcp, fit.ranges = F, fit.sills = F,
-                             vgm(model = "Sph"))
-    dat.krg.prcp <- krige(f.p, wx.day[!is.na(wx.day$precipIntensityMax),], grd, dat.fit)
-    
-    # Rasterize
-    prcp_r <- raster::raster(dat.krg.prcp)
-    prcp_r <- mask(prcp_r, grid_shp)
-    
-    # Now do tmin, tmax, and snow
-    f.tmin <- as.formula(temperatureMin ~ 1)
-    vg_tmin <- variogram(temperatureMin ~ 1, wx.day[!is.na(wx.day$temperatureMin),])
-    dat.fit <- fit.variogram(vg_tmin, fit.ranges = F, fit.sills = F,
-                             vgm(model = "Sph"))
-    dat.krg.tmin <- krige(f.tmin, wx.day[!is.na(wx.day$temperatureMin),], grd, dat.fit)
-    tmin_r <- raster::raster(dat.krg.tmin)
-    tmin_r <- mask(tmin_r, grid_shp)
-    
-    f.tmax <- as.formula(temperatureMax ~ 1)
-    vg_tmax <- variogram(temperatureMax ~ 1, wx.day[!is.na(wx.day$temperatureMax),])
-    dat.fit <- fit.variogram(vg_tmax, fit.ranges = F, fit.sills = F,
-                             vgm(model = "Sph"))
-    dat.krg.tmax <- krige(f.tmax, wx.day[!is.na(wx.day$temperatureMax),], grd, dat.fit)
-    tmax_r <- raster::raster(dat.krg.tmax)
-    tmax_r <- mask(tmax_r, grid_shp)
-    
-    # SNOW: model fitted with historical data had a variable called SNOW. Forecast does not, but does have precipType, so create SNOW from this.
-    wx.day@data <- wx.day@data %>%
-      mutate(SNOW = ifelse(precipType == 'snow', precipIntensity, 0))
-    
-    f.snow <- as.formula(SNOW ~ 1)
-    vg_snow <- variogram(SNOW ~ 1, wx.day[!is.na(wx.day$SNOW),])
-    dat.fit <- fit.variogram(vg_snow, fit.ranges = F, fit.sills = F,
-                             vgm(model = "Sph"))
-    dat.krg.snow <- krige(f.snow, wx.day[!is.na(wx.day$SNOW),], grd, dat.fit)
-    snow_r <- raster::raster(dat.krg.snow)
-    snow_r <- mask(snow_r, grid_shp)
-    
-    # Apply to grid cells in year-day ----
+# make points file for KNN 
+road_points <- roads %>% st_cast("POINT") %>% group_by(osm_id) %>% slice_head(n=1) %>% arrange(osm_id)
 
-    # Need to extract values from the raster layers to the polygons
-    prcp_extr <- raster::extract(x = prcp_r,   # Raster object
-                                 y = grid_shp, # SpatialPolygons
-                                 fun = mean,
-                                 df = TRUE)
+ID_geometry <- weather_hourly.proj %>% 
+  distinct(ID, .keep_all = T) %>% 
+  select(ID, geometry)
+  
+KNN <- as.data.frame(nn2(st_coordinates(ID_geometry), st_coordinates(road_points),  k=1, treetype = "kd", searchtype = "standard")$nn.id)
 
-    names(prcp_extr)[2] = "PRCP"
-    prcp_extr$ID = grid_shp$GRID_ID
-    
-    daily_result <- data.frame(day, prcp_extr)
-    
-    tmin_extr <- raster::extract(x = tmin_r,   # Raster object
-                                 y = grid_shp, # SpatialPolygons
-                                 fun = mean,
-                                 df = TRUE)
-    names(tmin_extr)[2] = "TMIN"
-    tmin_extr$ID = grid_shp$GRID_ID
-    
-    daily_result <- full_join(daily_result, tmin_extr)
-    
-    tmax_extr <- raster::extract(x = tmax_r,   # Raster object
-                                 y = grid_shp, # SpatialPolygons
-                                 fun = mean,
-                                 df = TRUE)
-    names(tmax_extr)[2] = "TMAX"
-    tmax_extr$ID = grid_shp$GRID_ID
-    
-    daily_result <- full_join(daily_result, tmax_extr)
-    
-    snow_extr <- raster::extract(x = snow_r,   # Raster object
-                                 y = grid_shp, # SpatialPolygons
-                                 fun = mean,
-                                 df = TRUE)
-    names(snow_extr)[2] = "SNOW"
-    snow_extr$ID = grid_shp$GRID_ID
-    
-    daily_result <- full_join(daily_result, snow_extr)
-    
-    # Save in temporary location in case the process is interrupted
-    fn = paste("Prep_Weather_Daily_", day,"_", g, ".RData", sep="")
-    
-    save(list="daily_result", file = file.path(inputdir,"Weather", fn))
-    
-    EndTime <- Sys.time()-StartTime
-    cat(as.character(day), 'completed', round(EndTime, 2), attr(EndTime, 'units'), '\n',
-        file = paste0("Prep_Weather_", g, "_log.txt"), append = T) 
-    
-    } # end if exists_fn
-    daily_result
-  } # end parallel loop
+weather_forecast <- road_points %>% 
+  st_drop_geometry() %>%
+  bind_cols(KNN) %>% 
+  mutate(ID = as.character(V1)) %>% 
+  full_join(weather_hourly, by= "ID", relationship="many-to-many") %>% 
+  select(osm_id, utc_hour, temperature, snowAccumulation, rainAccumulation)
    
-  EndTime <- Sys.time() - StartTime
-  cat(round(EndTime, 2), attr(EndTime, "units"), "\n")
-   
-  save(list = c("wx.grd.day"), 
-       file = file.path(inputdir, "Weather", prepname))
+  save(weather_forecast, file = file.path(inputdir, "Weather", prepname))
 
+  rm(KNN, ID_geometry, road_points, grd, queries, state_map, weather_hourly, weather_hourly.proj, wx_dat_i,
+     api_crs)
+  
   } else {
   load(file.path(inputdir, "Weather", prepname))
 }
 
+# left join this with the predict week df here or later? 
