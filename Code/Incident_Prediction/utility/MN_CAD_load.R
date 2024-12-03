0b2b8d1b1da7a6ab8f83e3baf34fd64750bcd664
Andrew Breck <31482159+ahbreck@users.noreply.github.com>
2024-12-03 00:10:41 -0500

    Few minor updates to address bugs, etc.

--- Code/Incident_Prediction/utility/MN_CAD_load.R
+++ Code/Incident_Prediction/utility/MN_CAD_load.R
@@ -1,11 +1,16 @@
 
+lookup <- c(CAD_STALL = "STALL", CAD_CRASH = "CRASH", CAD_HAZARD = "HAZARD", CAD_ROADWORK = "ROADWORK", CAD_None = "None")
+
 CAD <- read.csv(file.path(inputdir,"Crash",paste0(year, "_mndot_cadevents.csv"))) %>%
+  filter(lon != "None" & lat != "None") %>%
   mutate(lat=as.numeric(lat),
          lon=as.numeric(lon),
          centraltime=as.POSIXct(open,format="%m/%d/%Y %H:%M",tz="America/Chicago"),
          close = as.POSIXct(close,format="%m/%d/%Y %H:%M",tz="America/Chicago"),
          time_open = difftime(close, centraltime, units = "mins"),
-         month = lubridate::month(centraltime)) %>%
+         month = lubridate::month(centraltime),
+         day = lubridate::day(centraltime),
+         hour = lubridate::hour(centraltime)) %>%
   # some of the lat and lon values had the string "None," so above function resulted in NAs by coercion.
   # filter them out now
   filter(!is.na(lon)&!is.na(lat)) %>%
@@ -16,10 +21,21 @@ CAD <- read.csv(file.path(inputdir,"Crash",paste0(year, "_mndot_cadevents.csv"))
   # those are likely erroneous because they were closed so soon after being opened.
   filter(time_open > 5) %>%
   # select columns of interest
-  dplyr::select(pkey, eid, class, lat, lon, centraltime, time_open, location, month) %>%
+  dplyr::select(class, lat, lon, month, day, hour) %>%
   # convert to sf object
   st_as_sf(coords = c("lon","lat"), crs = 4326) %>%
   st_transform(projection) %>%
   # associated each record with the road segment it is nearest to
-  st_join(state_network, join = st_nearest_feature) %>%
-  st_drop_geometry()
+  st_join(state_network %>% select(osm_id), join = st_nearest_feature) %>%
+  st_drop_geometry() %>%
+  dplyr::select(osm_id, month, day, hour, class) %>% 
+  mutate(n = 1) %>%
+  group_by(osm_id, month, day, hour, class) %>%
+  summarize(n = sum(n)) %>%
+  pivot_wider(names_from = class, values_from = n)%>%
+  replace_na(list(STALL = 0, CRASH = 0, HAZARD = 0, ROADWORK = 0, None = 0)) %>%
+ rename(all_of(lookup))
+
+  
+  
+
