56c6c7cf2e7bead6660421b821ce50529233a093
Andrew Breck <31482159+ahbreck@users.noreply.github.com>
2024-12-23 12:28:28 -0500

    Update prep_other_year_crashes.R

--- Code/Incident_Prediction/utility/prep_other_year_crashes.R
+++ Code/Incident_Prediction/utility/prep_other_year_crashes.R
@@ -161,101 +161,50 @@ cat("Created total_crashes for other years")
 cat(round(timediff,2), attr(timediff, "unit"), "elapsed", "\n")
 
 #------------------------------------------------------------------------------------
+# split into month by month frames
+years <- unique(total_crashes$YEAR)
+if(!dir.exists(file.path(intermediatedir,'Month_Frames'))) { dir.create(file.path(intermediatedir,'Month_Frames')) }
 
-## Set up monthly training and test dataframes ----------------------
-
-# Switching to operate month-by-month to avoid running out of memory 
-# when trying to work with an entire year all at the same time.
-monthIDs <- formatC(1:12, width = 2, flag = "0")
-yearmonths <- c(paste(year, monthIDs, sep="-"))
-yearmonths.1 <- paste(yearmonths, "01", sep="-")
-#lastdays <- days_in_month(as.POSIXct(yearmonths.1)) # from lubridate
-#yearmonths.end <- paste(yearmonths, lastdays, sep="-")
-
-is_weekday <- function(timestamps){lubridate::wday(timestamps, week_start = 1) < 6}
+for(y in years){
+  temp_train = total_crashes %>% filter(YEAR = y)
+  # save the temp object
+  save(list = c('temp_train'), file = file.path(intermediatedir,'Month_Frames',paste(state, y, "month_frame_crashes_only", m,".RData", sep = "_")))
+}
 
 # uncomment for testing
 m <- 1
+y <- 2020
 
-for (m in 1:12){
-  starttime = Sys.time()
-  # create sequence of hours for the month
-  if(m<12){
-    dates <- seq(from = as.POSIXct(yearmonths.1[m]), to = as.POSIXct(yearmonths.1[m+1]), by = "hour")
-  } else {
-    dates <- seq(from = as.POSIXct(yearmonths.1[m]), to = as.POSIXct(c(paste(year+1, "01-01", sep="-"))), by = "hour")
-  }
-  # trim off the last hour, which is for the following month
-  dates <- head(dates, -1)
-  # convert into a dataframe with separate columns for month, day, hour, and day of week.
-  dates <- data.frame(dates = dates, 
-                      month = lubridate::month(dates), 
-                      day = lubridate::day(dates), 
-                      hour = lubridate::hour(dates), 
-                      day_of_week = lubridate::wday(dates, label = TRUE),
-                      weekday = is_weekday(dates))
-  # convert day_of_week into unordered since we want to treat it as a nominal variable. 
-  dates$day_of_week <- factor(dates$day_of_week, ordered = FALSE)
-  
-  # replicate it, multiplying by the number of road segments
-  dates_exp <- do.call(bind_rows, replicate(nrow(state_network), dates, simplify = FALSE)) %>%
-    select(!dates)
-  
-  # combine date/time columns from sample with osm_id and then crashes
-  # expand road segment frame, multiplying by the number of date/time training samples
-  temp_train <- do.call(bind_rows, replicate(nrow(dates), state_network %>% st_drop_geometry(), simplify = FALSE)) %>% 
-    arrange(osm_id) %>%
-    # merge with expanded version of date/time training frame
-    cbind(dates_exp, row.names=NULL) %>%
-    # bring in crash data
-    left_join(total_crashes, by = c('osm_id', 'month', 'day', 'hour')) %>%
-    mutate(crash = ifelse(is.na(crash), 0, crash))
-  
-  # save the temp object
-  if(!dir.exists(file.path(intermediatedir,'Month_Frames'))) { dir.create(file.path(intermediatedir,'Month_Frames')) }
-  
-  save(list = c('temp_train'), file = file.path(intermediatedir,'Month_Frames',paste(state, year, "month_frame", m,".RData", sep = "_")))
-  timediff = Sys.time() - starttime
-  
-  # clear memory
-  #rm(num_days, month, day, hour, dates, dates_exp, temp, sample, temp_train, temp_test)
-  
-  cat("Created training data frame with date/time, road segments, and crashes for month ", m, ". ")
-  cat(round(timediff,2), attr(timediff, "unit"), "elapsed", "\n")
-  gc()
-}
-
-rm(total_crashes, dates, dates_exp, datalist, crash_files, file_path, i, l)
+rm(datalist, crash_files, file_path, i, l)
 gc()
 #load(file = file.path(intermediatedir,'Month_Frames',paste(state, year, "month_frame", m,".RData", sep = "_")))
 
 ## waze data -----------------------------------------------
-
-# establish set of waze files that we'll be reading in
-waze.files <- list.files(path = waze_dir, pattern = "\\.csv$")
-
-waze.files.year <- file.path(inputdir, "Waze", state, waze.files[grep(as.character(year), waze.files)])
-
-waze.jams.files <- list.files(path = waze_jams_dir, pattern = "\\.csv$")
-
-waze.jams.files.year <- file.path(waze_jams_dir, waze.jams.files[grep(as.character(year), waze.jams.files)])
-
-# uncomment for testing
-m <- 1
-
-gc()
-
-# note that the waze_query.R script already converted to local time. If waze data were sourced in some other way, may need to
-# uncomment the parts that convert to local time below, from UTC.
 
 # If applicable, load CAD data for MN so as to join it in
 if((year %in% c(2018,2019,2020)) & (state == "MN")){
   source('utility/MN_CAD_load.R')
 }
 
+# establish set of waze files that we'll be reading in
+waze.files <- list.files(path = waze_dir, pattern = "\\.csv$")
+
+waze.jams.files <- list.files(path = waze_jams_dir, pattern = "\\.csv$")
+
+for(y in years){
+
+waze.files.year = file.path(inputdir, "Waze", state, waze.files[grep(as.character(y), waze.files)])
+
+waze.jams.files.year = file.path(waze_jams_dir, waze.jams.files[grep(as.character(y), waze.jams.files)])
+
+# uncomment for testing
+
+# note that the waze_query.R script already converted to local time. If waze data were sourced in some other way, may need to
+# uncomment the parts that convert to local time below, from UTC.
+
 for(m in 1:12){
   starttime = Sys.time()
-  load(file.path(intermediatedir,'Month_Frames',paste(state, year, "month_frame", m,".RData", sep = "_")))
+  load(file.path(intermediatedir,'Month_Frames',paste(state, y, "month_frame_crashes_only", m,".RData", sep = "_")))
   # read in data frames for that month
   waze_temp = read.csv(waze.files.year[m]) 
   waze_temp = waze_temp %>%
@@ -290,37 +239,37 @@ for(m in 1:12){
     temp_train = left_join(temp_train, CAD, by = c(osm_id, month, day, hour))
   }
   
-  # if jams data are available, read them in for that month
-  if(length(waze.jams.files.year) > 0){
-    waze_temp = read.csv(waze.jams.files.year[m]) %>%
-      rename(month = month_local,
-             day = day_local,
-             hour = hour_local) %>%
-      mutate(osm_id = as.character(osm_id))
-    
-    temp_train = temp_train %>% 
-      left_join(waze_temp %>% select(!pub_utc_timestamp_mean), by = c('osm_id', 'month', 'day', 'hour')) %>%
-      replace_na(list(level_mode = 0)) %>%
-      rename(jam_level = level_mode)
-  }
-  # create the imputed averages - two different versions depending on whether there are data on jams level
-  if(length(waze.jams.files.year) > 0){
-    waze_averages <- temp_train %>%
-      group_by(osm_id, month, weekday, hour) %>%
-      summarize(average_jams = mean(JAM),
-                average_weather = mean(WEATHERHAZARD),
-                average_closure = mean(ROAD_CLOSED),
-                average_accident = mean(ACCIDENT),
-                average_jam_level = mean(jam_level))
-    
-  } else {
+  # # if jams data are available, read them in for that month
+  # if(length(waze.jams.files.year) > 0){
+  #   waze_temp = read.csv(waze.jams.files.year[m]) %>%
+  #     rename(month = month_local,
+  #            day = day_local,
+  #            hour = hour_local) %>%
+  #     mutate(osm_id = as.character(osm_id))
+  #   
+  #   temp_train = temp_train %>% 
+  #     left_join(waze_temp %>% select(!pub_utc_timestamp_mean), by = c('osm_id', 'month', 'day', 'hour')) %>%
+  #     replace_na(list(level_mode = 0)) %>%
+  #     rename(jam_level = level_mode)
+  # }
+  # # create the imputed averages - two different versions depending on whether there are data on jams level
+  # if(length(waze.jams.files.year) > 0){
+  #   waze_averages <- temp_train %>%
+  #     group_by(osm_id, month, weekday, hour) %>%
+  #     summarize(average_jams = mean(JAM),
+  #               average_weather = mean(WEATHERHAZARD),
+  #               average_closure = mean(ROAD_CLOSED),
+  #               average_accident = mean(ACCIDENT),
+  #               average_jam_level = mean(jam_level))
+  #   
+  # } else {
     waze_averages <- temp_train %>%
       group_by(osm_id, month, weekday, hour) %>%
       summarize(average_jams = mean(JAM),
                 average_weather = mean(WEATHERHAZARD),
                 average_closure = mean(ROAD_CLOSED),
                 average_accident = mean(ACCIDENT))
-  }
+  #}
   
   # thin data, if needed, to avoid running out of memory when running Join_Road_Weather.R script
   if(!is.null(keep_prop)){
@@ -328,18 +277,16 @@ for(m in 1:12){
   }
   
   # save the objects
-  save(list = c('temp_train'), file = file.path(intermediatedir,'Month_Frames',paste(state, year, "month_frame_waze", m,".RData", sep = "_")))
-  save(waze_averages, file = file.path(intermediatedir,'Month_Frames',paste(state, year, "month_frame_imputed_waze", m,".RData", sep = "_")))
+  save(list = c('temp_train'), file = file.path(intermediatedir,'Month_Frames',paste(state, y, "month_frame_waze_crash_only", m,".RData", sep = "_")))
+  save(waze_averages, file = file.path(intermediatedir,'Month_Frames',paste(state, y, "month_frame_imputed_waze_crash_only", m,".RData", sep = "_")))
   timediff = Sys.time() - starttime
   
   # clear memory
   rm(temp_train, waze_temp, waze_averages)
   gc()
-  cat("Added waze data for month ", m, ". ")
+  cat("Added waze data for other years (just crash) for month ", m, ", year ", y, ". ")
   cat(round(timediff,2), attr(timediff, "unit"), "elapsed", "\n")
 }
-#load(file.path(intermediatedir,'Month_Frames',paste(state, year, "month_frame_waze", m,".RData", sep = "_")))
-#load(file = file.path(intermediatedir,'Month_Frames',paste(state, year, "month_frame_imputed_waze", m,".RData", sep = "_")))
 dif_time <- round(difftime(Sys.time(), top_time, units = "mins"), 2)
 cat(paste0("Created all month frames with crashes and waze data. ", dif_time, " minutes elapsed thus far in total."))
 cat("Now adding weather.")
@@ -349,6 +296,65 @@ gc()
 # Run Weather Script ------------------------------------------------------
 source(file.path("Utility", "Join_Road_weather.R"))
 
-rm(timezone_adj, tz_adjustments, US_timezones)
+#### Now apply the join_road_weather function for each month #####
+timeA <- Sys.time()
+
+for(m in 1:12){
+  gc()
+  innertime = Sys.time()
+  
+  # Do first half of the month first
+  load(file.path(intermediatedir,'Month_Frames',paste(state, y, "month_frame_waze_crash_only", m,".RData", sep = "_")))
+  
+  temp_train = temp_train %>% filter(day <= 15)
+  gc()
+  
+  temp_trainA = join_road_weather(temp_train)
+  rm(temp_train)
+  gc()
+  
+  save(temp_trainA, file = file.path(intermediatedir, 'Month_Frames', paste(state, y, m, 'month_frame_full_A_crash_only.Rdata', sep = "_")))
+  
+  rm(temp_trainA)
+  
+  gc()
+  
+  # then do second half of the month
+  load(file.path(intermediatedir,'Month_Frames',paste(state, y, "month_frame_waze_crash_only", m,".RData", sep = "_")))
+  
+  temp_train = temp_train %>% filter(day > 15)
+  gc()
+  
+  temp_train = join_road_weather(temp_train)
+  
+  gc()
+  
+  # load back up the first half of the month
+  load(file.path(intermediatedir, 'Month_Frames', paste(state, y, m, 'month_frame_full_A_crash_only.Rdata', sep = "_")))
+  
+  temp_train = bind_rows(temp_trainA, temp_train)
+  
+  save(temp_train, file = file.path(intermediatedir, 'Month_Frames', paste(state, y, m, 'month_frame_full_crash_only.Rdata', sep = "_")))
+  
+  rm(temp_train, temp_trainA)
+  
+  gc()
+  
+  dif_time <- round(difftime(Sys.time(), innertime, units = "mins"), 2)
+  cat(paste0("Added weather for month ", m, ", year ", y, ", in ", dif_time, " minutes."))
+  
+  dif_time <- round(difftime(Sys.time(), top_time, units = "mins"), 2)
+  cat(paste0(dif_time, " minutes elapsed thus far in total in the creation of training/test frames.\n\n"))
+}
+
+timeB <- Sys.time()
+dif_time <- round(difftime(timeB, timeA, units = "mins"), 2)
+cat(paste0("Weather Script took ", dif_time, " minutes."))
+rm(timeA, timeB, dif_time)
+
+# Remove variables and gc() before moving on
+rm(KNN, precip, road_points, snow, state_network_KNN, state_stations, stations, stations_KNN, temp, temp_df, temp_df_c, working_df, working_list, wx, xy, overall_time, precip_time, snow_time, starttime,
+   temp_time, year2, timezone_adj, tz_adjustments, US_timezones)
 
 gc()
+}
